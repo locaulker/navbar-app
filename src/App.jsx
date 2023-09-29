b5c6ac35fb5435f489a07c99317cfa291172e640
Lawrence Caulker <locaulker@gmail.com>
2023-09-29 02:36:29 -0400

    initial navbar setup complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
+import Navbar from "./Navbar"
+
 const App = () => {
-  return <h2>Navbar Starter</h2>;
-};
-export default App;
+  return (
+    <main>
+      <Navbar />
+    </main>
+  )
+}
+export default App
