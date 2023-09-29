ee0c95fb17674c02c2a832d66932f1c1a41b7085
Lawrence Caulker <locaulker@gmail.com>
2023-09-29 13:24:56 -0400

    complete navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -45,6 +45,18 @@ const Navbar = () => {
             })}
           </ul>
         </div>
+
+        {/* Social Links */}
+        <ul className="social-icons">
+          {social.map((socialIcon) => {
+            const { id, url, icon } = socialIcon
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            )
+          })}
+        </ul>
       </div>
     </nav>
   )
